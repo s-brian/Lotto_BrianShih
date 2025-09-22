01190c6204b169c6315e9dcf04c8c34506ce499f
s-brian <shih.brian123@gmail.com>
2025-09-22 16:07:53 +0800

    commit 9: short/long tap and coordinates (Brian Shih)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,18 +14,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
-
-import androidx.compose.runtime.setValue // 引入 setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-
+import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,32 +41,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
-    //var lucky = (1..100).random()
-    var lucky by remember {
-        mutableStateOf((1..100).random())
-    }
+    var lucky by remember { mutableStateOf((1..100).random()) }
+    var touchPosition by remember { mutableStateOf(Offset.Zero) }
 
-    val context = LocalContext.current // 取得當前 Context
+    val context = LocalContext.current
 
-    Column (modifier = modifier
-        .fillMaxSize()
-        .clickable {
-            Toast.makeText(context, "螢幕觸控(楊子青)", Toast.LENGTH_SHORT).show()
-        },
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .clickable{
+                Toast.makeText(context, "this is a text", Toast.LENGTH_SHORT).show()
+            }
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent(PointerEventPass.Initial)
+                        val position = event.changes.firstOrNull()?.position
+                        if (position != null) {
+                            touchPosition = position
+                        }
+                    }
+                }
+            },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-        ){
-        Text(
-            text = "樂透數字(1-100)為 $lucky"
-        )
+    ) {
+        Text(text = "樂透數字(1-100)為 $lucky",
+            modifier = Modifier.pointerInput(Unit){
+                detectTapGestures(
+                    onTap = {
+                        lucky += 1
+                        Toast.makeText(context, "+1", Toast.LENGTH_SHORT).show()
+                    },
+                    onLongPress = {
+                        lucky -= 1
+                        Toast.makeText(context, "-1", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            })
 
-        Button(
-            onClick = { lucky = (1..100).random() }
-        ) {
+
+        Button(onClick = { lucky = (1..100).random() }) {
             Text("重新產生樂透碼")
         }
+
+        Text(text = "X = ${touchPosition.x.toInt()}, Y = ${touchPosition.y.toInt()}")
     }
-
-
-
 }
